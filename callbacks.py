5b07cbf7cf8a34b13a283b5e339f8f184bf65136
djumanov <diyorbekjumanov1120@gmail.com>
2023-03-30 19:21:56 +0500

    like and dislike

--- callbacks.py
+++ callbacks.py
@@ -1,15 +1,28 @@
 from telegram.ext import CallbackContext
 from telegram import Update
+from db import DB
 
+db = DB()
 
 def start(update: Update, context: CallbackContext):
+    # add user into database
+    chat_id = update.message.chat.id
+    db.add(chat_id)
+    # send welcome message
     fullname = update.message.from_user.full_name
     update.message.reply_html(text=f'hello, <b>{fullname}</b>\n<i>Welcome to our bot!</i>')
 
 
 def like(update: Update, context: CallbackContext):
-    pass
-
+    # increase like
+    chat_id = update.message.chat.id
+    data = db.increase_like(chat_id)
+    # send number of your like and dislike
+    update.message.reply_html(text=f'like: {data.get("like")}\ndislike: {data.get("dislike")}')
 
 def dislike(update: Update, context: CallbackContext):
-    pass
+    # increase dislike
+    chat_id = update.message.chat.id
+    data = db.increase_dislike(chat_id)
+    # send number of your like and dislike
+    update.message.reply_html(text=f'like: {data.get("like")}\ndislike: {data.get("dislike")}')
